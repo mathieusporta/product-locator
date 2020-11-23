9c41f0d7f43f58b4600d69cfbe35e405106b5631
Beaucourt89 <beaucourt.pro@gmail.com>
2020-11-23 12:02:49 +0100

    init project with mongodb

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import React from "react";
+import styles from "../styles/Home.module.css";
+import { getDatabase } from "../src/database";
 
-export default function Home() {
-  return (
-   <h1>hello word</h1>
-  )
+const Home = ({ products }) => {
+  return <p>{JSON.stringify(products)}</p>;
+};
+
+export async function getServerSideProps() {
+  const mongodb = await getDatabase();
+
+  const products = await mongodb.db().collection("products").find().toArray();
+  console.log(products);
+
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
 }
+// JSON.parse(JSON.stringify(products)),
+export default Home;
