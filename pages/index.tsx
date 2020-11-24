ba4a376d17c92a391a081ddc7378dfc9b4bab242
Beaucourt89 <beaucourt.pro@gmail.com>
2020-11-24 12:26:53 +0100

    shop with div

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,66 @@ import React from "react";
 import styles from "../styles/Home.module.css";
 import { getDatabase } from "../src/database";
 
-const Home = ({ products }) => {
-  return <p>{JSON.stringify(products)}</p>;
+const Home = ({ products, shop }) => {
+  const styles = {
+    surface: {
+      padding: "20px",
+    },
+  };
+  return (
+    <>
+      <div>
+        <h1>Shop</h1>
+        {/* <p>{JSON.stringify(shop)}</p> */}
+
+        <hr />
+        <div className="magasin">
+          <div className="ligneDeCaisse">Caisse</div>
+          <div className="entree">Entrée</div>
+
+          {shop &&
+            shop.map((tata) => {
+              return (
+                <>
+                  <div style={styles.surface}>
+                    <div className="rayon" id={tata.slug}>
+                      {tata.name}
+
+                      {products &&
+                        products.map((toto) => {
+                          return toto.slug === "peinture" ? (
+                            <p>{toto.designation}</p>
+                          ) : null;
+                        })}
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export async function getServerSideProps() {
   const mongodb = await getDatabase();
 
-  const products = await mongodb.db().collection("products").find().toArray();
+  const shop = await mongodb.db().collection("shop").find().toArray();
+  // console.log(shop);
+  const products = await mongodb
+    .db()
+    .collection("products")
+    .find({ slug: "peinture" })
+    .toArray();
   console.log(products);
 
   return {
     props: {
+      shop: JSON.parse(JSON.stringify(shop)),
       products: JSON.parse(JSON.stringify(products)),
     },
   };
 }
-// JSON.parse(JSON.stringify(products)),
+
 export default Home;
--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,7 @@
 import { MongoClient } from "mongodb";
 const MONGODB_URI = process.env.MONGODB_URI;
+const baseOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+
 
 let cachedDb: MongoClient = null;
 
