337f89a9457efd5dcc64a2025b1909cda4d89856
mathieusporta <70898863+mathieusporta@users.noreply.github.com>
2020-11-24 17:23:21 +0100

    searchbarre (#17)
    Co-authored-by: jerome cnockaert <47606138+jeje59223@users.noreply.github.com>

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Header } from "./header";
+import  SearchBarSection  from "./SearchBarSection";
 import  Accueil  from "./accueil";
 
 export const Layout = ({ children }) => {
   return (
     <div>
       <Header />
+      <SearchBarSection/>
       <Accueil />
       {children}
     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import "../styles/SearchBar.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
