c7db2522c6e1fcd7eddff4c054da0d8ef3e6da97
Beaucourt89 <42272795+Beaucourt89@users.noreply.github.com>
2020-11-24 12:32:57 +0100

    shop with div rayon (#13)
    * init project with mongodb
    
    * shop with div

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,71 @@ import React from "react";
 import styles from "../styles/Home.module.css";
 import { getDatabase } from "../src/database";
 
+
+const Home = ({ products, shop }) => {
+  const styles = {
+    surface: {
+      padding: "20px",
+    },
+  };
+  return (
+    <>
+      <div>
+        <h1>Shop</h1>
+        {/* <p>{JSON.stringify(shop)}</p> */}
+
+        <hr />
+        <div className="magasin">
+          <div className="ligneDeCaisse">Caisse</div>
+          <div className="entree">Entrée</div>
+
+          {shop &&
+            shop.map((tata) => {
+              return (
+                <>
+                  <div style={styles.surface}>
+                    <div className="rayon" id={tata.slug}>
+                      {tata.name}
+
+                      {products &&
+                        products.map((toto) => {
+                          return toto.slug === "peinture" ? (
+                            <p>{toto.designation}</p>
+                          ) : null;
+                        })}
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+        </div>
+      </div>
+    </>
+  );
+    
 const Home = ({ products }) => {
   return <p>{JSON.stringify(products)}</p>;
+
 };
 
 export async function getServerSideProps() {
   const mongodb = await getDatabase();
+  const shop = await mongodb.db().collection("shop").find().toArray();
+  const products = await mongodb
+    .db()
+    .collection("products")
+    .find({ slug: "peinture" })
+    .toArray();
 
   const products = await mongodb.db().collection("products").find().toArray();
-  console.log(products);
+
 
   return {
     props: {
+      shop: JSON.parse(JSON.stringify(shop)),
       products: JSON.parse(JSON.stringify(products)),
     },
   };
 }
-// JSON.parse(JSON.stringify(products)),
+
 export default Home;
